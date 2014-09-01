e53325d1741cb31d8f88c6322df990fb1c215c8a
upbit <rmusique@gmail.com>
2014-09-01 23:42:00 +0800

    set UIFileSharingEnabled = true

--- examples/PixivDaily/PixivDaily/SDWebImageViewController.m
+++ examples/PixivDaily/PixivDaily/SDWebImageViewController.m
@@ -118,9 +118,6 @@
     self.widthZoomScale = self.view.bounds.size.width*_scrollView.maximumZoomScale / self.imageView.image.size.width;
     self.heightZoomScale = self.view.bounds.size.height*_scrollView.maximumZoomScale / self.imageView.image.size.height;
     
-    //NSLog(@"bound=%.2f scale=%.1f, image=%.2f", self.view.bounds.size.width, [UIScreen mainScreen].scale, self.imageView.image.size.width);
-    //NSLog(@"widthZoom=%.2f heightZoom=%.2f, minZoom=%.2f", self.widthZoomScale, self.heightZoomScale, minZoom);
-
     self.scrollView.zoomScale = self.widthZoomScale;
 }
 
