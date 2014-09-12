aa33ffb8a8aabda2d3288a33d8014840a49d70ac
upbit <rmusique@gmail.com>
2014-09-12 12:12:48 +0800

    export illusts to PhotosAlbum

--- examples/PixivDaily/PixivDaily/PixivImageViewController.m
+++ examples/PixivDaily/PixivDaily/PixivImageViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "PixivImageViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @interface PixivImageViewController ()
 
@@ -23,18 +24,20 @@
     return [documentsPath stringByAppendingPathComponent:name];
 }
 
-- (IBAction)exportIllustToDocuments:(UIBarButtonItem *)sender
+- (IBAction)exportIllust:(UIBarButtonItem *)sender
 {
     if ((!self.illust) || (self.illust.illustId == PIXIV_ID_INVALID))
         return;
     
-    NSString *illustName = [NSString stringWithFormat:@"illistid_%u.%@", (unsigned int)self.illust.illustId, self.illust.ext];
+    NSString *illustName = [NSString stringWithFormat:@"illist_id_%u.%@", (unsigned int)self.illust.illustId, self.illust.ext];
     NSString *illustPath = [self documentsPathForFileName:illustName];
     NSLog(@"export: %@", illustPath);
     
     __weak PixivImageViewController *weakSelf = self;
     dispatch_queue_t exportQueue = dispatch_queue_create("export illust", NULL);
     dispatch_async(exportQueue, ^{
+#if 0
+        // export to Documents/
         if ([weakSelf.illust.ext isEqualToString:@"png"]) {
             [UIImagePNGRepresentation(weakSelf.image) writeToFile:illustPath atomically:YES];
         } else {
@@ -49,6 +52,33 @@
                                                       otherButtonTitles:@"OK", nil];
             [alertView show];
         });
+#else
+        // export to Photos Album
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        [library writeImageToSavedPhotosAlbum:[weakSelf.image CGImage]
+                                  orientation:(ALAssetOrientation)[weakSelf.image imageOrientation]
+                              completionBlock:^(NSURL *assetURL, NSError *error) {
+                                  dispatch_async(dispatch_get_main_queue(), ^{
+                                      if (!error) {
+                                          UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Export Success!"
+                                                                                              message:illustName
+                                                                                             delegate:self
+                                                                                    cancelButtonTitle:nil
+                                                                                    otherButtonTitles:@"OK", nil];
+                                          [alertView show];
+                                      } else {
+                                          NSLog(@"export error: %@", [error localizedDescription]);
+                                          
+                                          UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Error!"
+                                                                                              message:[error localizedDescription]
+                                                                                             delegate:self
+                                                                                    cancelButtonTitle:nil
+                                                                                    otherButtonTitles:@"OK", nil];
+                                          [alertView show];
+                                      }
+                                  });
+                              }];
+#endif
     });
 }
 
